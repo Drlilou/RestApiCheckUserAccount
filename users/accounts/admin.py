a62933156abb9814f432d641748ceaf1e35a972e
Boudjema Ali <boudjema.ali94@gmail.com>
2024-05-24 10:40:53 +0100

    add accoun to admin panel

--- users/accounts/admin.py
+++ users/accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Account
 
-# Register your models here.
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('email', 'username', 'firstname', 'lastname','date_joined', 'is_active')
+    search_fields = ('email', 'username', 'firstname')
+    list_filter = ('is_active', 'date_joined')
+    ordering = ('-date_joined',)
--- users/users/settings.py
+++ users/users/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'accounts',
-]
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
